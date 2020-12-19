0c2301f01fd14c596ebcfd5ff0a853feea5ffbbf
Igor Malyarov <igor1309@gmail.com>
2020-12-19 20:49:46 +0300

    Initial Commit

---
+++ RBizReportApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RBizReportApp
+//
+//  Created by Igor Malyarov on 19.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RBizReportApp/RBizReportAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RBizReportAppApp.swift
+//  RBizReportApp
+//
+//  Created by Igor Malyarov on 19.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct RBizReportAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
